9f44c7ffb2a24f7f3be066fb976876eaf1b30445
Sham nadeem khan <marketing@vdengg.com>
2024-10-09 18:08:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='student-login'>
+      <div className='left-side'>
+        <div className='logo'>
+          <img src='./vdei-logo.png' alt='logo'></img>
+        </div>
+        <div className='contact-form'>
+          <form>
+            <div className='login-id'>
+              <label>
+                Login ID<br></br>
+                <input type="text" name="login-id" placeholder="Enter Your Id...." />
+              </label>
+            </div>
+            <div className='login-password'>
+              <label>
+                Login Password<br></br>
+                <input type="text" name="login-password" placeholder="Enter Your Password...." />
+              </label>
+            </div>
+            <div className='forget-password'>
+              <span>Forget Password?</span>
+            </div>
+            <div className='submit-btn'>
+              <input type="submit" value="Submit" />
+              <img src='./arrow.png' alt='arrow'></img>
+            </div>
+            <div className='help'>
+              <span>Need Any help?? Contact Us...</span>
+            </div>
+            <div className='social-logo'>
+              <img src='./mail.png' alt='mail'></img>
+              <img src='./whatsapp.png' alt='whatsapp'></img>
+              <img src='./call.png' alt='call'></img>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className='right-side'>
+        <div className='banner-img'>
+          <img src='./student-login-banner-img.png' alt='banner-img'></img>
+        </div>
+      </div>
     </div>
   );
 }
